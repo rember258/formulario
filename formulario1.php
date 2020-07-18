f42bde7b8ccebc423c0a93be9fe06ea2a16eb3b4
rember258 <remberccenua258@gmail.com>
2020-07-18 07:33:27 -0500

    primeros formularios

---
+++ formulario1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rember</title>
+</head>
+<body>
+<form method="post" action="registro.php">
+Nombre    : <input type="text" name="nombre" /><br />
+Direccion : <input type="text" name="direccion" /><br />
+
+<input type="submit" name="enviar" value="Aceptar informacion" />
+</form>
+</body>
+</html>
